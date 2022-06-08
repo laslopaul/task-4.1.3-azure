9eb8d8ed38258dc034d25a5e0a7c799a49ae4251
laslopaul <96680549+laslopaul@users.noreply.github.com>
2022-06-08 15:17:33 +0300

    fix: change public ip properties

--- networks.tf
+++ networks.tf
@@ -4,6 +4,15 @@ resource "azurerm_resource_group" "rg" {
   location = "japaneast"
 }
 
+# Create public IP for app gateway
+resource "azurerm_public_ip" "publicip_appgw" {
+  name                = "PublicIP_AppGw"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "Standard"
+  allocation_method   = "Static"
+}
+
 # Create virtual network
 resource "azurerm_virtual_network" "vnet0" {
   name                = "VNET01"
@@ -18,12 +27,5 @@ resource "azurerm_subnet" "appgw_subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet0.name
   address_prefixes     = ["10.0.1.0/24"]
-}
-
-# Create public IP for app gateway
-resource "azurerm_public_ip" "publicip_appgw" {
-  name                = "PublicIP_AppGw"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  service_endpoints    = ["Microsoft.Web"]
 }
